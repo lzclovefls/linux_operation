8b794d75fbd04e4358cf53aba436c62fb8355fb8
lzc <2563958505@qq.com>
2018-12-11 13:57:12 +0800

    update command.sh

--- command.sh
+++ command.sh
@@ -10,11 +10,11 @@
 
 
 #获取操作命令
-read -p '\n\n请先输入命令首字母:' a 
+read -p '请先输入命令首字母:' a 
 
 case $a in
     p) 
-    read -p "\n\n请输入要查找的字符串:" b 
+    read -p "请输入要查找的字符串:" b 
     if [ -n $b ]
     then
         c=$(echo "$b"|sed 's/^ //g'|sed 's/ $//g')
