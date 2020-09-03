bbf7f189e6af425081e80222822f75d5e47f0777
lianzhengcan <2563958505@qq.com>
2020-09-03 16:54:53 +0800

    Update backupDb.sh

--- backupDb.sh
+++ backupDb.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-#日志所在文件夹的绝对路径
+#备份所在文件夹的绝对路径
 dir=/www/backup/database
 
 dbname=xiaoshuo            #数据库名称
 username=xiaoshuo          #用户名
 password=tLeKLXGe8kKTHMhf  #密码
 
+#对应数据库备份所在文件夹路径
+backup_dir=${backup_dir}/${dbnmae}
+
 #数据库备份
-mysqldump -u${username} -p${password} ${dbname} | xz > ${dir}/${dbname}_$(date +%Y%m%d_%H%M%S).sql.xz
+mysqldump -u${username} -p${password} ${dbname} | xz > ${backup_dir}/$(date +%Y%m%d_%H%M%S).sql.xz
 
 #备份时间
 days=30
 
 #找出并删除备份时间之前的文件（防止备份数据占用磁盘空间过大）
-find ${dir} -name '*.sql.xz'  -mtime +${days}  -type f  -delete
+find ${backup_dir} -name '*.sql.xz'  -mtime +${days}  -type f  -delete
